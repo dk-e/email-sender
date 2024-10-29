b3da7535ea36a8c7b282ca946309fb358d8ee3a7
dan <d@zurly.me>
2024-10-29 22:20:37 +0000

    frick this man

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import EmailForm from "@/components/email-form";
 import { redirect } from "next/navigation";
 
-export default async function Home(
-  props: {
-    searchParams: Promise<{ [pass: string]: string | undefined }>;
-  }
-) {
+export default async function Home(props: {
+  searchParams: { [pass: string]: string | undefined };
+}) {
   const searchParams = await props.searchParams;
   try {
     if (searchParams.pass !== process.env.ACCESS_PASSWORD) redirect("/fail");
